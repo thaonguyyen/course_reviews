2ce030775e18d12aa7a0737a16dec3ec4364da6a
kelzzhou <ews9rk@virginia.edu>
2023-05-01 23:16:25 -0400

    added more to GUI

--- src/main/java/org/example/CourseReviewController.java
+++ src/main/java/org/example/CourseReviewController.java
@@ -8,25 +8,43 @@ import javafx.scene.control.TextField;
 
 public class CourseReviewController {
     private CourseReviewImplementation implementation;
+    private Student logginInStudent;
 
     @FXML
     private Label passwordsDontMatch;
     @FXML
     private TextField username, password;
     @FXML
-    private Scene loginScene, mainMenuScene;
+    private Scene loginScene, mainMenuScene, seeReviewsScene, addReviewScene;
     @FXML
     private Button createNewUser, logIn;
     @FXML
     public void initialize(){
 
         implementation = new CourseReviewImplementation();
+//        username = new TextField("Username: ");
+//        password = new TextField("Password: ");
+//        createNewUser = new Button("Create New User");
+//        logIn = new Button("Login");
+        //logInScene.setRoot()
+
+    }
+    private void logIn(){
         username = new TextField("Username: ");
         password = new TextField("Password: ");
         createNewUser = new Button("Create New User");
         logIn = new Button("Login");
-        //logInScene.setRoot()
+        createNewUser.setOnAction(e->{
+            createNewUser();
+        });
+        logIn.setOnAction(event -> {checkLogIn();});
+    }
+    private void createNewUser(){
 
+    }
+    private void checkLogIn(){
+
+        //implementation.login(username, password);
     }
     private void passwordsDontMatch(){
         passwordsDontMatch.setText("Incorrect username or password. Please try again");
