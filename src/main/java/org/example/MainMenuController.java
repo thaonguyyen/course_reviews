6fdd0bbfdf8e7639798c27c8012b8f5a63b37ef6
bryantlisk <rhq6nu@virginia.edu>
2023-05-03 16:53:54 -0400

    Made button actions switch screens, although it should still need to be connected to the actual buttons

--- src/main/java/org/example/MainMenuController.java
+++ src/main/java/org/example/MainMenuController.java
@@ -3,9 +3,11 @@ package org.example;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Screen;
 
 public class MainMenuController {
     private CourseReviewImplementation implementation;
+    private ScreenManager screenManager;
     @FXML
     private Button submitReview, seeReviews, logout;
     @FXML
@@ -13,6 +15,7 @@ public class MainMenuController {
     @FXML
     public void initialize(){
         implementation = CourseReviewImplementation.getInstance();
+        screenManager = ScreenManager.getInstance();
         title.setText("Main Menu");
         prompt.setText("Please select an option below");
         submitReview.setText("Submit Review");
@@ -20,13 +23,17 @@ public class MainMenuController {
         logout.setText("Logout");
         submitReview.setOnAction(e->{
             //switch to submit review scene
+            screenManager.switchScreen("submit review");
+
         });
         seeReviews.setOnAction(e->{
             //switch to see review scene
+            screenManager.switchScreen("see reviews");
         });
         logout.setOnAction(e->{
             implementation.logout();
             //switch to welcome screen
+            screenManager.switchScreen("welcome");
         });
     }
 }
