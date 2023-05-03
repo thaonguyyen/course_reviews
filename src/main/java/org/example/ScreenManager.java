c2edde019041917cbf4d0189e45dffbdcfdf01a3
bryantlisk <rhq6nu@virginia.edu>
2023-05-03 16:42:59 -0400

    Made screenmanager into a singleton so every class can access all the scenes associated with it.

--- src/main/java/org/example/ScreenManager.java
+++ src/main/java/org/example/ScreenManager.java
@@ -5,13 +5,24 @@ import javafx.scene.layout.Pane;
 
 import java.util.HashMap;
 
-//Used this solution for switching scenes
+//Used this solution for switching scenes, but made it a singleton so it's easier to pass around controller classes
 //https://stackoverflow.com/questions/37200845/how-to-switch-scenes-in-javafx
 public class ScreenManager {
+    private static ScreenManager instance;
     HashMap<String, Pane> screens = new HashMap<String, Pane>();
     private Scene main;
 
-    public ScreenManager(Scene main){
+    private ScreenManager(){
+    }
+
+    public static ScreenManager getInstance(){
+        if(instance == null){
+            instance = new ScreenManager();
+        }
+        return instance;
+    }
+
+    public void initializeMainScene(Scene main){
         this.main = main;
     }
 
