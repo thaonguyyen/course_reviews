0bb8452f99e53aaf332e44e999d8614f2945345f
bryantlisk <rhq6nu@virginia.edu>
2023-05-03 17:06:36 -0400

    Made WelcomeController's buttons switch screens once they are properly connected

--- src/main/java/org/example/WelcomeController.java
+++ src/main/java/org/example/WelcomeController.java
@@ -10,19 +10,23 @@ public class WelcomeController {
     @FXML
     private Label welcome, prompt;
     private CourseReviewImplementation implementation;
+    private ScreenManager screenManager;
 
     @FXML
     public void initialize(){
-//        implementation = new CourseReviewImplementation();
-//        welcome.setText("Welcome!");
-//        prompt.setText("Please select an option below to get started");
-//        logInToExistingUser.setText("Loging-to an Existing User");
-//        createNewUser.setText("Create a New User");
+        implementation = CourseReviewImplementation.getInstance();
+        screenManager = ScreenManager.getInstance();
+        welcome.setText("Welcome!");
+        prompt.setText("Please select an option below to get started");
+        logInToExistingUser.setText("Loging-to an Existing User");
+        createNewUser.setText("Create a New User");
         logInToExistingUser.setOnAction(e->{
             //switch to login scene
+            screenManager.switchScreen("login");
         });
         createNewUser.setOnAction(e->{
             //switch to create new user scene
+            screenManager.switchScreen("new user");
         });
     }
 }
